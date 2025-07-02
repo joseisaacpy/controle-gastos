3758b2dafb795d37d694c3d846782be55a3640b5
José Isaac <joseisaacnascimento@gmail.com>
2025-07-02 20:13:52 -0300

    feat: Adiciona loading pra esperar os dados serem recebidos da api - Adiciona uma div com input de data de início e fim, pra poder filtrar o dashboard

--- public/client/js/dashboard.js
+++ public/client/js/dashboard.js
@@ -3,6 +3,15 @@ async function getInfoToDash() {
   const spanTotal = document.getElementById("totalGasto");
   const spanTabulacao = document.getElementById("totalTabulacao");
   const spanGastoMedio = document.getElementById("gastoMedio");
+  const spanLastGasto = document.getElementById("ultimoGasto");
+
+  //   Para loading
+  const loading = document.getElementById("loading");
+  const cards = document.getElementById("cardsContainer");
+
+  // Mostra o loading e esconde os cards
+  loading.style.display = "flex";
+  cards.classList.add("hidden");
 
   //   Consumindo a API
   try {
@@ -25,18 +34,51 @@ async function getInfoToDash() {
     const totalGastas = gastos.reduce((acumulador, iAtual) => {
       return acumulador + iAtual.valor;
     }, 0);
+
     // Adiciona o valor total formatado no span
     spanTotal.innerText = new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL",
     }).format(totalGastas);
+
+    // Pega a média de gastos e joga no span
+    const gastoMedio = totalGastas / gastos.length;
     spanGastoMedio.innerText = new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL",
-    }).format(totalGastas / gastos.length);
+    }).format(gastoMedio);
+
+    // Pega última tabulação e joga no span
+    if (gastos.length > 0) {
+      const lastGasto = gastos[gastos.length - 1];
+      const valorFormatado = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(lastGasto.valor);
+      const dataFormatada = new Date(lastGasto.data).toLocaleDateString(
+        "pt-BR"
+      );
+
+      spanLastGasto.innerHTML = `
+    <span class="flex flex-col font-bold">
+      <span>Categoria: ${lastGasto.categoria}</span>
+      <span>Valor: ${valorFormatado}</span>
+      <span>Data: ${dataFormatada}</span>
+    </span>
+  `;
+    } else {
+      spanLastGasto.innerText = "Nenhum gasto registrado.";
+    }
+
     // Se tiver erro, mostra o erro no console
   } catch (error) {
     console.log(error);
+    loading.textContent = "Erro ao carregar os dados.";
+  } finally {
+    // Esconde o loading e mostra os cards
+    loading.style.display = "none";
+    cards.classList.remove("hidden");
+    cards.classList.add("grid");
   }
 }
 
